f8f61e3df925e1c7d09391fa0776b5a53034ed90
faceslog <66552663+faceslog@users.noreply.github.com>
2021-07-16 22:34:12 +0200

    Update PartIdentifiers.cpp

--- PartIdentifiers/PartIdentifiers.cpp
+++ PartIdentifiers/PartIdentifiers.cpp
@@ -10,7 +10,7 @@
 
 #include "WinDisk.h"
 
-int main()
+int DisplayMenu(const std::vector<std::wstring>& drives, int exit)
 {
     std::wcout << std::endl;
     std::wcout << "Faceslog - Disk Helper" << std::endl;
@@ -19,36 +19,86 @@ int main()
     std::wcout << "Github: www.github.com/faceslog" << std::endl;
     std::wcout << std::endl;
 
-    for (auto& drive : WinDisk::GetValidDrives())
+    std::wcout << "Select a Drive to Query Informations: " << std::endl;
+    for (size_t i{ 0 }; i < drives.size(); i++)
     {
-        std::wcout << std::endl;
-        std::wcout << "=======================================================" << std::endl;
-        std::wcout << L"Drive: " << drive << std::endl;
+        std::wcout << '[' << i << "] - " << drives.at(i) << std::endl;
+    }
 
-        std::wstring vGuid = WinDisk::GetVolumeGuid(drive);
+    // Add supplementary options
+    std::wcout << '[' << drives.size() << "] - " << "To Refresh Drive List" << std::endl;
+    std::wcout << '[' << exit << "] - " << "To Exit The Program" << std::endl;
+
+    int x;
+    do
+    {
+        std::wcout << "Input: ";
+        std::cin >> x;
+        std::cout << std::endl;
+
+    } while (x < 0 || x > exit);
+
+    return x;
+}
+
+void DisplayDiskInfo(const std::wstring& drive)
+{
+    std::wcout << std::endl;
+    std::wcout << L"============================ DISK INFO: ==========================" << std::endl;
+    std::wcout << L"Drive: " << drive << std::endl;
+
+    std::wstring vGuid = WinDisk::GetVolumeGuid(drive);
+    // Check that the GUID is not empty
+    if (!vGuid.empty())
+    {
         std::wcout << L"GUID: " << vGuid << std::endl;
-        
         for (auto& nb : WinDisk::GetDiskNumbers(vGuid))
         {
-            std::wcout << "Disk Numbers: " << nb << std::endl;
-           
+            std::wcout << L"Disk Numbers: " << nb << std::endl;
+
             auto part = WinDisk::GetPartList(nb);
 
             for (size_t i{ 0 }; i < part.size(); i++)
             {
-                std::wcout << "-----------------------" << std::endl;
-                std::wcout << "Partition " << (i + 1) << ':' << std::endl;
+                std::wcout << L"-----------------------" << std::endl;
+                std::wcout << L"Partition " << (i + 1) << ':' << std::endl;
                 WinDisk::DisplayPartDetail(part.at(i));
-                
+
             }
 
         }
-        std::wcout << "=======================================================" << std::endl;
-        std::wcout << std::endl;
     }
+    else
+    {
+        std::wcout << L"An error occured with that drive, did you unplugged it ?" << std::endl;
+    }
+        
+    std::wcout << L"==================================================================" << std::endl;
+    std::wcout << std::endl;
+}
 
-    std::cout << "Press any key to exit this window ..." << std::endl;
-    (void) std::getchar();
+int main()
+{    
+    while (true)
+    {
+        auto drives = WinDisk::GetValidDrives();
+
+        int exit = drives.size() + 1;
+        auto result = DisplayMenu(drives, exit);
+
+        // Refresh the drives list
+        if (result == drives.size())
+            continue;
+        // Exit the program
+        if (result == (exit)) 
+            break;
+
+        DisplayDiskInfo(drives.at(result));
+    }
+    
+    std::wcout << "Thanks for using me :)" << std::endl;
+
+    system("pause");
 
     return EXIT_SUCCESS;
 }
